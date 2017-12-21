1f905d1e257b7e3c0f1578f4be035e3cd38b604e
userpixel <hanifbbz@gmail.com>
2017-12-22 00:48:35 +0100

    use a mutex to operate on counter

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"math/big"
 	"os"
 	"strconv"
+	"sync"
 )
 
 var big0 = big.NewInt(0)
@@ -13,25 +14,26 @@ var big1 = big.NewInt(1)
 func factorial(x int) *big.Int {
 	vals := make(chan *big.Int, x+1)
 	res := make(chan *big.Int)
-	count := make(chan int)
+	count := x
+	var countMutex sync.Mutex
 	for i := 1; i <= x; i++ {
 		j := big.NewInt(int64(i))
 		vals <- j
 		go func() {
-			c := <-count
-			if c == 0 {
+			countMutex.Lock()
+			count--
+			countMutex.Unlock()
+			if count == 0 {
 				res <- <-vals
 			} else {
 				a := <-vals
 				b := <-vals
-				count <- c - 1
 				c := new(big.Int)
 				c.Mul(a, b)
 				vals <- c
 			}
 		}()
 	}
-	count <- x - 1
 	return <-res
 }
 
@@ -39,11 +41,10 @@ func main() {
 	if len(os.Args) != 2 {
 		panic("We need exactly one argument which is numerical")
 	}
-	nStr := os.Args[1]
-	nParsed, parsingError := strconv.Atoi(nStr)
+	xStr := os.Args[1]
+	x, parsingError := strconv.Atoi(xStr)
 	if parsingError != nil {
-		panic(fmt.Sprintf("Failed to parse %s %T as an integer number because %s", nStr, nStr, parsingError))
+		panic(fmt.Sprintf("Failed to parse %s %T as an integer number because %s", xStr, xStr, parsingError))
 	}
-	fmt.Printf("%d!\n", nParsed)
-	fmt.Println(factorial(nParsed))
+	fmt.Printf("%d! = %d", x, factorial(x))
 }
