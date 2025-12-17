ce3834e0365c7bf60b8fe0e353aebf0fbd840c45
Alex Ewerlöf <alex.ewerlof@gmail.com>
2025-12-17 22:24:08 +0100

    replace panics with os.Exit()

--- main.go
+++ main.go
@@ -8,15 +8,18 @@ import (
 
 func main() {
 	if len(os.Args) != 2 {
-		panic("We need exactly one argument")
+		fmt.Println("We need exactly one argument")
+		os.Exit(1)
 	}
 	xStr := os.Args[1]
 	x, parsingError := strconv.Atoi(xStr)
 	if parsingError != nil {
-		panic(fmt.Sprintf("Failed to parse %s %T as an integer number because %s", xStr, xStr, parsingError))
+		fmt.Printf("Failed to parse %s %T as an integer number because %s\n", xStr, xStr, parsingError)
+		os.Exit(1)
 	}
 	if x < 2 {
-		panic("The number should be bigger than 1")
+		fmt.Println("The number should be bigger than 1")
+		os.Exit(1)
 	}
 	result := factorial(uint64(x))
 	fmt.Println("Converting to string...")
