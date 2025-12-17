4e5e158da77a411260d250399aaba3e9a3ae72e4
Alex Ewerlöf <alex.ewerlof@gmail.com>
2025-12-17 22:43:58 +0100

    remove dead test

--- factorize_test.go
+++ factorize_test.go
@@ -4,24 +4,6 @@ import (
 	"testing"
 )
 
-// Prime numbers under 100
-var primes = []uint64{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}
-
-// deepEqualSliceUint64 tests whether two slices contain the same elements.
-func deepEqualSliceUint64(slice1, slice2 []uint64) bool {
-	if len(slice1) != len(slice2) {
-		return false
-	}
-
-	for i, v := range slice1 {
-		if v != slice2[i] {
-			return false
-		}
-	}
-
-	return true
-}
-
 // deepEqualPPMap tests whether two PPMap structs contain the same elements.
 func deepEqualPPMap(ppMap1, ppMap2 PriPow) bool {
 	if len(ppMap1) != len(ppMap2) {
@@ -35,22 +17,6 @@ func deepEqualPPMap(ppMap1, ppMap2 PriPow) bool {
 	}
 
 	return true
-}
-
-func TestPrimeFactors(t *testing.T) {
-	knownResults := map[uint64][]uint64{
-		2:  {2},
-		6:  {2, 3},
-		13: {13},
-		24: {2, 2, 2, 3},
-	}
-
-	for n, expected := range knownResults {
-		result := primeFactors(n, primes)
-		if !deepEqualSliceUint64(result, expected) {
-			t.Errorf("Could not break %d to its primes\nExpected: %v\nGot:     %v", n, expected, result)
-		}
-	}
 }
 
 func TestFactorize(t *testing.T) {
