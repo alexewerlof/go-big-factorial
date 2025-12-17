7dc51cc0ce20ea1832bc8a38075f0f06ffd00514
Alex Ewerlöf <alex.ewerlof@gmail.com>
2025-12-17 22:23:51 +0100

    bugfix in prime array

--- primes.go
+++ primes.go
@@ -15,7 +15,14 @@ func isPrime(x uint64, primes []uint64) bool {
 }
 
 func primesUnder(max uint64) []uint64 {
-	primes := []uint64{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}
+	initialPrimes := []uint64{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}
+	var primes []uint64
+	for _, p := range initialPrimes {
+		if p >= max {
+			return primes
+		}
+		primes = append(primes, p)
+	}
 
 	for i := uint64(101); i < max; i += 2 {
 		if i%5 == 0 {
